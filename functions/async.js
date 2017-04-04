e7d6525b076829e187adec0d4e941e1908022fff
Frank McCready <frank.mccready@grainger.com>
2017-04-04 09:39:41 -0500

    Add comments

--- functions/async.js
+++ functions/async.js
@@ -1,7 +1,13 @@
 function analytics(){
+  // Scoped variables
   var top = false;
   var bottom = false;
   var dataTop, dataBottom;
+  /* 
+    Return function that tests if both the top and bottom have returned data or whatever else you want to test for.
+    If it passes the test. Do something with the data each returned.
+    You can also but a timeout here and do something based on the data you have received if any.
+  */
   return function(location, data){
     if (location === 'top'){
       top = true;
@@ -19,11 +25,15 @@ function analytics(){
   }
 }
 var test = analytics();
+
+// Function for random time intervals
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+// Timeout to simulate the first async request.
 setTimeout(function(){
   console.log('TOP IS DONE');
   var data = {
@@ -33,7 +43,7 @@ setTimeout(function(){
   test('top', data);
 }, getRandomInt(1, 5000));
 
-
+// Timeout to simulate the second async request.
 setTimeout(function(){
   console.log('BOTTOM IS DONE');
   var data = {
